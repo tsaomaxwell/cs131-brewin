99b4e7f3fa85203e3bf84dccd67708e86c9f595d
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2023-10-20 04:36:31 -0700

    29/30

--- interpreterv1.py
+++ interpreterv1.py
@@ -14,6 +14,8 @@ class Interpreter(InterpreterBase):
         ast = parse_program(program)
         self.variables_values = {}
         main_func_node = self.get_main_func_node(ast)
+        if self.trace_output:
+            print(main_func_node)
         if main_func_node is not None:
             self.run_main_func(main_func_node)
         else:
@@ -23,36 +25,38 @@ class Interpreter(InterpreterBase):
             )
 
     def get_main_func_node(self, ast):
-        if ast['elem_type'] == InterpreterBase.PROGRAM_DEF:
-            if 'functions' in ast['dict']:
-                if 'main' in ast['dict']['functions']:
-                    return ast['dict']['functions']['main']
+        if ast.elem_type == InterpreterBase.PROGRAM_DEF:
+            for func_node in ast.dict['functions']:
+                if func_node.dict['name'] == 'main':
+                    return func_node
         return None
     
     def run_main_func(self, func_node):
-        if func_node['elem_type'] == InterpreterBase.FUNC_DEF:
-            statements = func_node['statements']
+        if func_node.elem_type == InterpreterBase.FUNC_DEF:
+            statements = func_node.dict['statements']
             for statement in statements:
                 self.run_statement(statement)
 
     def run_statement(self, statement_node):
-        if statement_node['elem_type'] == '=':
-            key = statement_node['dict']['name']
-            right_node = statement_node['dict']['expression']
+        if statement_node.elem_type == '=':
+            key = statement_node.dict['name']
+            right_node = statement_node.dict['expression']
             self.variables_values[key] = self.evaluate_exp_var_or_val(right_node)
-        elif statement_node['elem_type'] == InterpreterBase.FCALL_DEF:
-            pass
+        elif statement_node.elem_type == InterpreterBase.FCALL_DEF:
+            func_name = statement_node.dict['name']
+            args = statement_node.dict['args']
+            self.run_func(func_name, args)
     
     def evaluate_exp_var_or_val(self, node):
-        if node['elem_type'] == InterpreterBase.INT_DEF or node['elem_type'] == InterpreterBase.STRING_DEF:
-            return copy.copy(node)
-        elif node['elem_type'] == InterpreterBase.VAR_DEF:
+        if node.elem_type == InterpreterBase.INT_DEF or node.elem_type == InterpreterBase.STRING_DEF:
+            return node
+        elif node.elem_type == InterpreterBase.VAR_DEF:
             return self.evaluate_var(node)
         else:
             return self.evaluate_expression(node)
     
     def evaluate_var(self, var_node):
-        var_name = var_node['dict']['name']
+        var_name = var_node.dict['name']
         if var_name in self.variables_values:
             return copy.copy(self.variables_values[var_name])
         else:
@@ -62,39 +66,39 @@ class Interpreter(InterpreterBase):
             )
     
     def evaluate_expression(self, expression_node):
-        if expression_node['elem_type'] == '+':
-            op1 = self.evaluate_exp_var_or_val(expression_node['dict']['op1'])
-            op2 = self.evaluate_exp_var_or_val(expression_node['dict']['op2'])
+        if expression_node.elem_type == '+':
+            op1 = self.evaluate_exp_var_or_val(expression_node.dict['op1'])
+            op2 = self.evaluate_exp_var_or_val(expression_node.dict['op2'])
             return self.evaluate_add(op1, op2)
-        elif expression_node['elem_type'] == '-':
-            op1 = self.evaluate_exp_var_or_val(expression_node['dict']['op1'])
-            op2 = self.evaluate_exp_var_or_val(expression_node['dict']['op2'])
+        elif expression_node.elem_type == '-':
+            op1 = self.evaluate_exp_var_or_val(expression_node.dict['op1'])
+            op2 = self.evaluate_exp_var_or_val(expression_node.dict['op2'])
             return self.evaluate_subtract(op1, op2)
-        elif expression_node['elem_type'] == InterpreterBase.FCALL_DEF:
-            func_name = expression_node['dict']['name']
-            args = expression_node['dict']['args']
+        elif expression_node.elem_type == InterpreterBase.FCALL_DEF:
+            func_name = expression_node.dict['name']
+            args = expression_node.dict['args']
             return self.run_func(func_name, args)
     
     def evaluate_add(self, op1, op2):
-        if op1['elem_type'] == InterpreterBase.STRING_DEF or op2['elem_type'] == InterpreterBase.STRING_DEF:
+        if op1.elem_type == InterpreterBase.STRING_DEF or op2.elem_type == InterpreterBase.STRING_DEF:
             super().error(
                 ErrorType.TYPE_ERROR,
                 "Incompatible types for add operation",
             )
         else:
             result = copy.copy(op1)
-            result['dict']['val'] = op1['dict']['val'] + op2['dict']['val']
+            result.dict['val'] = op1.dict['val'] + op2.dict['val']
             return result
         
     def evaluate_subtract(self, op1, op2):
-        if op1['elem_type'] == InterpreterBase.STRING_DEF or op2['elem_type'] == InterpreterBase.STRING_DEF:
+        if op1.elem_type == InterpreterBase.STRING_DEF or op2.elem_type == InterpreterBase.STRING_DEF:
             super().error(
                 ErrorType.TYPE_ERROR,
                 "Incompatible types for subtract operation",
             )
         else:
             result = copy.copy(op1)
-            result['dict']['val'] = op1['dict']['val'] - op2['dict']['val']
+            result.dict['val'] = op1.dict['val'] - op2.dict['val']
             return result
     
     def run_func(self, func_name, args):
@@ -102,11 +106,14 @@ class Interpreter(InterpreterBase):
             return self.handle_inputi(args)
         elif func_name == 'print':
             self.handle_print(args)
+        else:
+            super().error(ErrorType.NAME_ERROR,
+                      f"Unknown function {func_name}")
 
     def handle_inputi(self, args):
         if len(args) == 1:
             value = self.evaluate_exp_var_or_val(args[0])
-            prompt = value['dict']['val']
+            prompt = value.dict['val']
             super().output(prompt)
         elif len(args) > 1:
             super().error(
@@ -119,9 +126,13 @@ class Interpreter(InterpreterBase):
         return input_element
     
     def handle_print(self, args):
+        if self.trace_output:
+            print('handle_print', args)
         string_to_output = ''
         for arg in args:
             value = self.evaluate_exp_var_or_val(arg)
-            output_piece = value['dict']['val']
+            output_piece = value.dict['val']
             string_to_output += str(output_piece)
+        if self.trace_output:
+            print(string_to_output)
         super().output(string_to_output)
